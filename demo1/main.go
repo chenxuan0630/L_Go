05a29f1738dab9b7390f622a43383ef1f6b11718
chenxuan0630 <15959815071@163.com>
2024-04-25 11:52:42 +0800

    学习了go语言中的整数类型

--- demo1/main.go
+++ demo1/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// 全局变量可以采用一次性声明
+var (
+	username = "xuan"
+	password = 159951
+)
+
 func main() {
-	fmt.Println("This is demo1")
+	// 变量练习
+	var test int
+	fmt.Println(test) //输出0
+
+	// 省略var
+	sex := "男"
+	fmt.Println(sex)
+
+	// 同时赋予多个变量值
+	var n1, n2, name = 100, 1000, "xuan"
+	fmt.Println(n1)
+	fmt.Println(n2)
+	fmt.Println(name)
+
+	fmt.Println(username)
+	fmt.Println(password)
 }
--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+
+	//字符串换行
+	fmt.Println("hellooooooooooooooooooooooooo",
+		"ooooooooooooooooooooooooooooooooooooooooo",
+		"opoooooooooooooooooooooooooooooooooooooo")
+
 }
